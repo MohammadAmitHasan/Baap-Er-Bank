3933f80a434cb66e131241b1c6383176a70828e4
MohammadAmitHasan <bhamithasan@gmail.com>
2022-02-10 17:10:29 +0600

    Responsive done

--- JS/banking.js
+++ JS/banking.js
@@ -1,3 +1,4 @@
+// Deposit event handle
 document.getElementById('deposit-btn').addEventListener('click', function () {
     // Get the new deposit amount
     const newDeposit = document.getElementById('deposit-input');
@@ -15,3 +16,22 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     // Clear the deposit field
     newDeposit.value = '';
 });
+
+// Withdraw event handle
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    // Get the withdraw amount
+    const newWithdrawInput = document.getElementById('withdraw-input');
+    const withdrawValue = newWithdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawValue);
+    // Get the previous withdraw total
+    const withdrawTotal = document.getElementById('withdraw-amount');
+    const withdrawTotalValue = parseFloat(withdrawTotal.innerText);
+    const withdrawTotalAmount = withdrawAmount + withdrawTotalValue;
+    // Set the new withdraw value
+    withdrawTotal.innerText = withdrawTotalAmount;
+    //update Balance
+    const balance = document.getElementById('total-balance');
+    balance.innerText = parseFloat(balance.innerText) - withdrawAmount;
+    // Clear withdraw input
+    newWithdrawInput.value = '';
+})
